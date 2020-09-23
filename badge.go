e5039a4d08b75be9d81ee525892bf6897489468d
Maartje Eyskens <maartje@eyskens.me>
2020-09-23 15:49:45 +0200

    Inital rewrite to tinygo 0.15

---
+++ badge.go
@@ -0,0 +1,191 @@
+package main
+
+import (
+	"image/color"
+	"machine"
+	"time"
+
+	"tinygo.org/x/tinydraw"
+	"tinygo.org/x/tinyfont"
+	"tinygo.org/x/tinyfont/freesans"
+	"tinygo.org/x/tinyfont/gophers"
+)
+
+const (
+	WIDTH  = 160
+	HEIGHT = 128
+)
+
+const (
+	BLACK = iota
+	WHITE
+	RED
+)
+
+var colors = []color.RGBA{
+	color.RGBA{0, 0, 0, 255},
+	color.RGBA{255, 255, 255, 255},
+	color.RGBA{255, 0, 0, 255},
+}
+
+var rainbow []color.RGBA
+var pressed uint8
+var quit bool
+
+func Badge() {
+	quit = false
+	display.FillScreen(colors[BLACK])
+
+	rainbow = make([]color.RGBA, 256)
+	for i := 0; i < 256; i++ {
+		rainbow[i] = getRainbowRGB(uint8(i))
+	}
+
+	for {
+		scroll("This badge", "runs", "TINYGO")
+		if quit {
+			break
+		}
+		myNameIsRainbow("@maartjeme")
+		if quit {
+			break
+		}
+		// blinkyRainbow("technologist", "FOR HIRE")
+		// if quit {
+		// 	break
+		// }
+		scroll("Go Dev Room", "9:00 Sun.", "UB2.252A")
+		if quit {
+			break
+		}
+		// scroll("@_conejo", "10:30 Sun.", "Go devroom")
+		// if quit {
+		// 	break
+		// }
+		// scroll("My talk", "16:00 Sun.", "IoT devroom")
+		// if quit {
+		// 	break
+		// }
+	}
+}
+
+func myNameIs(name string) {
+	display.FillScreen(colors[WHITE])
+
+	var r int16 = 8
+
+	// black corners detail
+	display.FillRectangle(0, 0, r, r, colors[BLACK])
+	display.FillRectangle(0, HEIGHT-r, r, r, colors[BLACK])
+	display.FillRectangle(WIDTH-r, 0, r, r, colors[BLACK])
+	display.FillRectangle(WIDTH-r, HEIGHT-r, r, r, colors[BLACK])
+
+	// round corners
+	tinydraw.FilledCircle(&display, r, r, r, colors[RED])
+	tinydraw.FilledCircle(&display, WIDTH-r-1, r, r, colors[RED])
+	tinydraw.FilledCircle(&display, r, HEIGHT-r-1, r, colors[RED])
+	tinydraw.FilledCircle(&display, WIDTH-r-1, HEIGHT-r-1, r, colors[RED])
+
+	// top band
+	display.FillRectangle(r, 0, WIDTH-2*r-1, r, colors[RED])
+	display.FillRectangle(0, r, WIDTH, 26, colors[RED])
+
+	// bottom band
+	display.FillRectangle(r, HEIGHT-r-1, WIDTH-2*r-1, r+1, colors[RED])
+	display.FillRectangle(0, HEIGHT-2*r-1, WIDTH, r, colors[RED])
+
+	// top text : my NAME is
+	w32, _ := tinyfont.LineWidth(&freesans.Regular12pt7b, "my NAME is")
+	tinyfont.WriteLine(&display, &freesans.Regular12pt7b, (WIDTH-int16(w32))/2, 24, "my NAME is", colors[WHITE])
+
+	// middle text
+	w32, _ = tinyfont.LineWidth(&freesans.Bold9pt7b, name)
+	tinyfont.WriteLine(&display, &freesans.Bold9pt7b, (WIDTH-int16(w32))/2, 72, name, colors[BLACK])
+
+	// gophers
+	tinyfont.WriteLineColors(&display, &gophers.Regular32pt, WIDTH-48, 110, "BE", []color.RGBA{getRainbowRGB(100), getRainbowRGB(200)})
+}
+
+func myNameIsRainbow(name string) {
+	myNameIs(name)
+
+	w32, _ := tinyfont.LineWidth(&freesans.Bold9pt7b, name)
+	for i := 0; i < 120; i++ {
+		tinyfont.WriteLineColors(&display, &freesans.Bold9pt7b, (WIDTH-int16(w32))/2, 72, name, rainbow[i:])
+		pressed, _ = buttons.Read8Input()
+		if pressed&machine.BUTTON_SELECT_MASK > 0 {
+			quit = true
+			break
+		}
+	}
+}
+
+func blinky(topline, bottomline string) {
+	display.FillScreen(colors[WHITE])
+
+	// calculate the width of the text so we could center them later
+	w32top, _ := tinyfont.LineWidth(&freesans.Bold12pt7b, topline)
+	w32bottom, _ := tinyfont.LineWidth(&freesans.Bold12pt7b, bottomline)
+	for i := int16(0); i < 10; i++ {
+		// show black text
+		tinyfont.WriteLine(&display, &freesans.Bold12pt7b, (WIDTH-int16(w32top))/2, 50, topline, colors[BLACK])
+		tinyfont.WriteLine(&display, &freesans.Bold12pt7b, (WIDTH-int16(w32bottom))/2, 100, bottomline, colors[BLACK])
+
+		// repeat the other way around
+		tinyfont.WriteLine(&display, &freesans.Bold12pt7b, (WIDTH-int16(w32top))/2, 50, topline, colors[WHITE])
+		tinyfont.WriteLine(&display, &freesans.Bold12pt7b, (WIDTH-int16(w32bottom))/2, 100, bottomline, colors[WHITE])
+
+		pressed, _ = buttons.Read8Input()
+		if pressed&machine.BUTTON_SELECT_MASK > 0 {
+			quit = true
+			break
+		}
+	}
+}
+
+func blinkyRainbow(topline, bottomline string) {
+	display.FillScreen(colors[WHITE])
+
+	// calculate the width of the text so we could center them later
+	w32top, _ := tinyfont.LineWidth(&freesans.Bold12pt7b, topline)
+	w32bottom, _ := tinyfont.LineWidth(&freesans.Bold12pt7b, bottomline)
+	for i := int16(0); i < 20; i++ {
+		// show black text
+		tinyfont.WriteLine(&display, &freesans.Bold12pt7b, (WIDTH-int16(w32top))/2, 50, topline, getRainbowRGB(uint8(i*12)))
+		tinyfont.WriteLine(&display, &freesans.Bold12pt7b, (WIDTH-int16(w32bottom))/2, 100, bottomline, getRainbowRGB(uint8(i*12)))
+
+		pressed, _ = buttons.Read8Input()
+		if pressed&machine.BUTTON_SELECT_MASK > 0 {
+			quit = true
+			break
+		}
+	}
+}
+
+func scroll(topline, middleline, bottomline string) {
+	display.FillScreen(colors[WHITE])
+
+	// calculate the width of the text so we could center them later
+	w32top, _ := tinyfont.LineWidth(&freesans.Bold12pt7b, topline)
+	w32middle, _ := tinyfont.LineWidth(&freesans.Bold12pt7b, middleline)
+	w32bottom, _ := tinyfont.LineWidth(&freesans.Bold12pt7b, bottomline)
+	tinyfont.WriteLine(&display, &freesans.Bold12pt7b, (WIDTH-int16(w32top))/2, 34, topline, getRainbowRGB(200))
+	tinyfont.WriteLine(&display, &freesans.Bold12pt7b, (WIDTH-int16(w32middle))/2, 60, middleline, getRainbowRGB(80))
+	tinyfont.WriteLine(&display, &freesans.Bold12pt7b, (WIDTH-int16(w32bottom))/2, 100, bottomline, getRainbowRGB(120))
+
+	display.SetScrollArea(0, 0)
+	for k := 0; k < 4; k++ {
+		for i := int16(159); i >= 0; i-- {
+
+			pressed, _ = buttons.Read8Input()
+			if pressed&machine.BUTTON_SELECT_MASK > 0 {
+				quit = true
+				break
+			}
+			display.SetScroll(i)
+			time.Sleep(10 * time.Millisecond)
+		}
+	}
+	display.SetScroll(0)
+	display.StopScroll()
+}
---
+++ main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"image/color"
+	"machine"
+	"time"
+
+	"tinygo.org/x/drivers/buzzer"
+	"tinygo.org/x/drivers/lis3dh"
+
+	"tinygo.org/x/drivers/ws2812"
+
+	"tinygo.org/x/drivers/shifter"
+	"tinygo.org/x/drivers/st7735"
+)
+
+var display st7735.Device
+var buttons shifter.Device
+var leds ws2812.Device
+var bzr buzzer.Device
+var accel lis3dh.Device
+var snakeGame Game
+
+func main() {
+	machine.SPI1.Configure(machine.SPIConfig{
+		SCK:       machine.SPI1_SCK_PIN,
+		SDO:       machine.SPI0_SDO_PIN,
+		SDI:       machine.SPI0_SDI_PIN,
+		Frequency: 8000000,
+	})
+	machine.I2C0.Configure(machine.I2CConfig{SCL: machine.SCL_PIN, SDA: machine.SDA_PIN})
+
+	accel = lis3dh.New(machine.I2C0)
+	accel.Address = lis3dh.Address0
+	accel.Configure()
+	println(accel.Connected())
+
+	display = st7735.New(machine.SPI1, machine.TFT_RST, machine.TFT_DC, machine.TFT_CS, machine.TFT_LITE)
+	display.Configure(st7735.Config{
+		Rotation: st7735.ROTATION_90,
+	})
+
+	buttons = shifter.New(shifter.EIGHT_BITS, machine.BUTTON_LATCH, machine.BUTTON_CLK, machine.BUTTON_OUT)
+	buttons.Configure()
+
+	neo := machine.NEOPIXELS
+	neo.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	leds = ws2812.New(neo)
+
+	snakeGame = Game{
+		colors: []color.RGBA{
+			color.RGBA{0, 0, 0, 255},
+			color.RGBA{0, 200, 0, 255},
+			color.RGBA{250, 0, 0, 255},
+			color.RGBA{160, 160, 160, 255},
+		},
+		snake: Snake{
+			body: [208][2]int16{
+				{0, 3},
+				{0, 2},
+				{0, 1},
+			},
+			length:    3,
+			direction: 3,
+		},
+		appleX: -1,
+		appleY: -1,
+		status: START,
+	}
+
+	for {
+		switch menu() {
+		case 0:
+			Badge()
+			break
+		case 1:
+			snakeGame.Start()
+			break
+		case 2:
+			Leds()
+			break
+		case 3:
+			Accel3D()
+			break
+		default:
+			break
+		}
+		println("LOOP")
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func getRainbowRGB(i uint8) color.RGBA {
+	if i < 85 {
+		return color.RGBA{i * 3, 255 - i*3, 0, 255}
+	} else if i < 170 {
+		i -= 85
+		return color.RGBA{255 - i*3, 0, i * 3, 255}
+	}
+	i -= 170
+	return color.RGBA{0, i * 3, 255 - i*3, 255}
+}
